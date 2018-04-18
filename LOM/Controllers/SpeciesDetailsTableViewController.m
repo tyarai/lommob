6c0c6b5b8a581517dd329c567b29642f4ba8aa66
Ranto Andrianavonison <ranto@tyarai.com>
2018-04-18 22:31:03 +0300

    Species Details : Vita ny screen#1- Hiditra @ Details

---
+++ LOM/Controllers/SpeciesDetailsTableViewController.m
@@ -0,0 +1,348 @@
+//
+//  SpeciesDetailsTableViewController.m
+//  LOM
+//
+//  Created by Ranto Tiaray Andrianavonison on 18/04/2018.
+//  Copyright © 2018 Kerty KAMARY. All rights reserved.
+//
+
+#import "SpeciesDetailsTableViewController.h"
+#import "Tools.h"
+#import "Constants.h"
+#import "Photographs.h"
+#import "Maps.h"
+#import "UIImage+Resize.h"
+#import "ScientificName.h"
+
+
+#define _ROWHEIGHT 48
+#define _fixedPhotoCellHeight 311
+#define _fixedMenuCellHeight 55
+
+@interface SpeciesDetailsTableViewController ()
+
+@end
+
+@implementation SpeciesDetailsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = _ROWHEIGHT;
+    
+    
+    if(self.specy){
+        [self prepareImageScroll];
+        self.scientificName.text = self.specy._title;
+    }
+}
+
+
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self initTimer];
+}
+
+- (void) viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    
+    if ([_timerScrollImage isValid]) {
+        [_timerScrollImage invalidate];
+    }
+    _timerScrollImage = nil;
+}
+
+
+- (void) initTimer{
+    _imagePosition = 0;
+    
+    _timerScrollImage = [NSTimer scheduledTimerWithTimeInterval:3.0
+                                                         target:self
+                                                       selector:@selector(showNextImage)
+                                                       userInfo:nil
+                                                        repeats:YES];
+    
+    
+    self.sliderImageControl.userInteractionEnabled = YES;
+    [self.sliderImageControl addTarget:self action:@selector(changeLemurPhoto) forControlEvents:UIControlEventValueChanged];
+    self.sliderImageControl.hidesForSinglePage = YES;
+    
+}
+
+- (void) changeLemurPhoto{
+    
+    _imagePosition = self.sliderImageControl.currentPage;
+    [self showImageAtPosition:_imagePosition];
+    
+}
+
+- (void) showImageAtPosition: (NSInteger) position{
+    
+    _imagePosition = position;
+    
+    [self.scrollImage setContentOffset:CGPointMake(_imagePosition * [Tools getScreenWidth], 0) animated:YES];
+    
+    self.sliderImageControl.currentPage = _imagePosition;
+}
+
+#pragma UIScrollViewdelegate
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    
+    if (_lastContentOffset < (int)scrollView.contentOffset.x) {
+        // moved right
+        if (self.sliderImageControl.currentPage < _photographs.count)
+        {
+            self.sliderImageControl.currentPage = self.sliderImageControl.currentPage + 1;
+        }
+        else
+        {
+            self.sliderImageControl.currentPage = self.sliderImageControl.currentPage;
+        }
+    }
+    else if (_lastContentOffset > (int)scrollView.contentOffset.x) {
+        // moved left
+        if (self.sliderImageControl.currentPage > 0) {
+            self.sliderImageControl.currentPage = self.sliderImageControl.currentPage - 1;
+        }else{
+            self.sliderImageControl.currentPage = self.sliderImageControl.currentPage;
+        }
+    }
+    
+    [self showImageAtPosition:self.sliderImageControl.currentPage];
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    _lastContentOffset = scrollView.contentOffset.x;
+}
+
+- (void) showPhotoBrowser{
+    
+    
+    self.photos = [NSMutableArray array];
+    
+    
+    for (Photographs* photograph in _photographs) {
+        
+        if (![Tools isNullOrEmptyString:photograph._photograph]) {
+            
+            /*NSString* imageBundleName = [NSString stringWithFormat:@"%@.jpg", photograph._photograph];
+             
+             [self.photos addObject:[MWPhoto photoWithImage:[UIImage imageNamed:imageBundleName]]];
+             */
+            
+            //NSString* imageBundleName = [NSString stringWithFormat:@"%@.jpg", photograph._photograph];
+            
+            NSString* imageBundleName = photograph._photograph;
+            
+            
+            UIImage * image = [Tools loadImage:imageBundleName];
+            
+            if(image == nil) {
+                //TODO: Atao ilay sary default_specy no sary aseho
+                // image = default_specy
+            }
+            
+            [self.photos addObject:[MWPhoto photoWithImage:image]];
+            
+            
+            
+        }else {
+            
+            [self.photos addObject:[MWPhoto photoWithImage:[UIImage imageNamed:@"ico_default_specy"]]];
+            
+        }
+    }
+    
+    
+    browser = [[MWPhotoBrowser alloc] initWithDelegate:self];
+    
+    // Set options
+    browser.displayActionButton = YES; // Show action button to allow sharing, copying, etc (defaults to YES)
+    browser.displayNavArrows = YES; // Whether to display left and right nav arrows on toolbar (defaults to NO)
+    browser.displaySelectionButtons = NO; // Whether selection buttons are shown on each image (defaults to NO)
+    browser.zoomPhotosToFill = YES; // Images that almost fill the screen will be initially zoomed to fill (defaults to YES)
+    browser.alwaysShowControls = NO; // Allows to control whether the bars and controls are always visible or whether they fade away to show the photo full (defaults to NO)
+    browser.enableGrid = NO; // Whether to allow the viewing of all the photo thumbnails on a grid (defaults to YES)
+    browser.startOnGrid = NO; // Whether to start on the grid of thumbnails instead of the first photo (defaults to NO)
+    browser.autoPlayOnAppear = NO; // Auto-play first video
+    
+    [browser setCurrentPhotoIndex:_imagePosition];
+    
+    
+    
+    // Present
+    [self.navigationController pushViewController:browser animated:YES];
+    
+}
+
+#pragma mark MWPhotoBrowserDelegate
+
+- (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser{
+    return self.photos.count;
+}
+
+- (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index{
+    if (index < self.photos.count) {
+        return [self.photos objectAtIndex:index];
+    }
+    return nil;
+}
+
+
+
+- (void) showNextImage{
+    
+    
+    if (_photographs && _photographs.count > 0) {
+        _imagePosition++;
+        
+        if (_imagePosition >= _photographs.count) {
+            _imagePosition = 0;
+        }
+        
+        [self showImageAtPosition:_imagePosition];
+        
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath{
+    if (indexPath.row == 0) {
+        return UITableViewAutomaticDimension;
+    }else if (indexPath.row == 1){
+        return _fixedPhotoCellHeight;
+    }
+    return _fixedMenuCellHeight;
+}
+
+- (void) prepareImageScroll{
+    
+    _imagePosition = 0;
+    
+    int x = 0;
+    int y = 0;
+    
+    float width = [Tools getScreenWidth];
+    float height = 300;
+    
+    _photographs = [self.specy getSpeciePhotographs];
+    
+    if (_photographs != nil && _photographs.count > 0) {
+        
+        if (_photographs.count == 1) {
+            self.sliderImageControl.numberOfPages = 1;
+            [self.sliderImageControl setHidden:YES];
+        }else{
+            self.sliderImageControl.numberOfPages = _photographs.count;
+            [self.sliderImageControl setHidden:NO];
+        }
+        
+        for (Photographs* photograph in _photographs) {
+            
+            UIImageView* imageView = [[UIImageView alloc] initWithFrame:CGRectMake(x, y, width , height)];
+            // imageView.contentMode = UIViewContentModeScaleAspectFill;
+            imageView.contentMode = UIViewContentModeScaleToFill;
+            
+            if (![Tools isNullOrEmptyString:photograph._photograph]) {
+                
+                //NSString* imageBundleName = [NSString stringWithFormat:@"%@.jpg", photograph._photograph];
+                
+                NSString* imageBundleName =  photograph._photograph;
+                
+                UIImage *image = [Tools loadImage:imageBundleName];
+                
+                CGSize newSize = CGSizeMake(width,height);
+                UIImage *scaledImage = [image scaledImageToSize:newSize];
+                [imageView setImage:scaledImage];
+                
+            }else{
+                [imageView setImage:[UIImage imageNamed:@"ico_default_specy"]];
+            }
+            
+            [self.scrollImage addSubview:imageView];
+            x += [Tools getScreenWidth];
+            
+        }
+        
+        [self.scrollImage setContentSize:CGSizeMake(x, 0)];
+    }
+    
+}
+
+
+/*
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+ */
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
